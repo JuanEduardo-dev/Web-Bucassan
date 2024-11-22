94ddcfda4b1a6861b3cb79e2287ddef2059e19d5
SplintrX <jngmzrjs@gmail.com>
2024-11-22 18:21:51 -0500

    Avance 2.5

--- bucassan-web/src/app/layout.tsx
+++ bucassan-web/src/app/layout.tsx
@@ -1,8 +1,12 @@
+'use client';
+
+import { useEffect } from 'react';
+import { clarity } from 'react-microsoft-clarity';
+
 import { Navbar } from '@/components/layout/Navbar';
 import { Footer } from '@/components/layout/Footer';
 import { ButtonWhatsapp } from '@/components/layout/ButtonWhatsapp';
 import { LandbotComponent } from '@/components/layout/LandbotComponent';
-import { ClarityIntegration } from '@/components/layout/ClarityIntegration';
 import "./globals.css";
 import "./fonts.css";
 
@@ -11,11 +15,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    clarity.init('p2rj0iq37x'); // Reemplaza 'your-id' con tu ID de Clarity
+  }, []);
+
   return (
     <html lang="es">
       <head>
         <title>Bucassan | Clínica Odontológica</title>
-        <ClarityIntegration />
       </head>
       <body className="min-h-screen flex flex-col">
         <Navbar />
