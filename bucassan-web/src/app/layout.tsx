c296d8f7f8327e22ec30d1dd06cda2578306d11d
SplintrX <jngmzrjs@gmail.com>
2024-11-23 17:56:44 -0500

    Keywords

--- bucassan-web/src/app/layout.tsx
+++ bucassan-web/src/app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
       <head>
         <meta name="google-site-verification" content="XLPWUF9_PDhbgtFfYvjX0zv36TtDiV55x-vCy8oJuq4" />
         <title>Bucassan | Clínica Odontológica</title>
+        <meta name="description" content="Recupera la confianza en tu sonrisa. En Buccasan brindamos un servicio de alta calidad con profesionales de amplia experiencia para que obtengas la sonrisa que siempre soñaste. Nuestros tratamientos de diseño de sonrisa pueden incluir: Carillas dentales, Blanqueamiento dental, Ortodoncia y mucho más. Reserva tu cita aquí +51 974 980 380. Nos encontramos en Jr. Los Laureles # 328 Paucarbambilla - Amarilis. Detrás de una linda sonrisa, hay una boca sana." />
+        <meta name="keywords" content="carillas dentales, blanqueamiento dental, ortodoncia, clínica dental, Buccasan, sonrisa perfecta, salud bucal, cita dental, profesionales dentales, tratamientos dentales" />
       </head>
       <body className="min-h-screen flex flex-col">
         <Navbar />
